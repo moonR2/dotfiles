abf84a004a276a375aab6ef5455ea47adad2b319
moonR2 <moonyneverend@gmail.com>
2021-07-09 13:32:03 -0500

    awesomeWM config

---
+++ .config/awesome/configuration/scratchpad.lua
@@ -0,0 +1,41 @@
+local awful = require("awful")
+local bling = require("module.bling")
+local beautiful = require("beautiful")
+local dpi = beautiful.xresources.apply_dpi
+local awestore = require("awestore")
+
+local anim_x = awestore.tweened(-1010, {
+    duration = 300,
+    easing = awestore.easing.cubic_in_out
+})
+
+local music_scratch = bling.module.scratchpad:new{
+    command = music,
+    rule = {instance = "music"},
+    sticky = false,
+    autoclose = false,
+    floating = true,
+    geometry = {x = dpi(10), y = dpi(606), height = dpi(460), width = dpi(960)},
+    reapply = true,
+    awestore = {x = anim_x}
+}
+
+awesome.connect_signal("scratch::music", function() music_scratch:toggle() end)
+
+local anim_y = awestore.tweened(1090, {
+    duration = 300,
+    easing = awestore.easing.cubic_in_out
+})
+
+local chat_scratch = bling.module.scratchpad:new{
+    command = [[ firefox -P chat --new-tab -url https://discord.com/channels/@me --new-tab -url https://app.element.io --class chat ]],
+    rule = {class = "chat"},
+    sticky = false,
+    autoclose = false,
+    floating = true,
+    geometry = {x = dpi(460), y = dpi(90), height = dpi(900), width = dpi(1000)},
+    reapply = true,
+    awestore = {y = anim_y}
+}
+
+awesome.connect_signal("scratch::chat", function() chat_scratch:toggle() end)
